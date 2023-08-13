7e0bfa6a7787a666ef39b89b550462082d2fef3a
fahmidanial <naloniel2000@gmail.com>
2023-08-13 17:45:46 +0800

    end

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -18,6 +18,8 @@ class Settings():
         
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
         
         self.initialize_dynamic_settings
         
@@ -31,6 +33,15 @@ class Settings():
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
         
+    def increase_speed(self):
+        """ Increase speed settings and alien point values. """
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
+        
     def initialize_dynamic_settings(self):
         """ Initialize settings that change throughout the game. """
         self.ship_speed_factor = 1.5
@@ -40,11 +51,10 @@ class Settings():
         # fleet_direction of 1 represents right; -11 rep[resents left.
         self.fleet_direction = 1
         
-    def increase_speed(self):
-        """ Increase speed settings. """
-        self.ship_speed_factor *= self.speedup_scale
-        self.bullet_speed_factor *= self.speedup_scale
-        self.alien_speed_factor *= self.speedup_scale
+        # Scoring
+        self.alien_points = 50
+        
+    
         
         
         
